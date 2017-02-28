34d4eb8e8a2d0a6bad098f3694da15aa41641686
wilon <wilonx@163.com>
2017-02-28 23:09:10 +0800

    some note

--- GitNote.php
+++ GitNote.php
@@ -7,32 +7,6 @@ id_rsa.pub私钥
 
 ## 克隆一个项目
 git clone https://github.com/Wilon/mynote.git
-## 命令起别名
-git config --global alias.bak 'reset --hard HEAD~1'
-git config --global alias.st status
-git config --global alias.ll 'log --graph'
-
-## 查看一些信息
-git status    # 项目目录里
-git branch -a    # 查看所有分支，*代表本地
-gti diff 文件    # 当期文件修改
-git log    # 查看提交日志
-git log -p    # 查看提交日志，包含代码
-git log --graph    # 以图表形式查看分支提交日志
-git show    # 查看最近一次提交代码
-git show commit_id   # 查看某一次提交代码
-
-## 添加文件
-git add mynewsfile/*
-
-## 提交到本地库
-git commit -a -m '说明'    # 类vim选择，去掉#选择
-
-## 从远程库拉取
-git pull
-
-## 提交到远程库
-git push origin 库名
 
 # 回退所有内容到上一个版本
 git reset --hard HEAD~1
